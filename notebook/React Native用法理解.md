03a16a068418ff07e64bb3ea46dee794e5cfb536
胡安静 <huaj5@spdb.com.cn>
2020-01-16 13:58:24 +0800

    完成对Promise对象的整理

--- notebook/React Native用法理解.md
+++ notebook/React Native用法理解.md
@@ -56,6 +56,19 @@ then(
 })
 ```
 
+注意，调用resolve或reject并不会终结 Promise 的参数函数的执行。这是因为立即 resolved 的 Promise 是在本轮事件循环的末尾执行，总是晚于本轮循环的同步任务。
+```
+new Promise((resolve, reject) => {
+  resolve(1);
+  console.log(2);
+}).then(r => {
+  console.log(r);
+});
+=====
+>>2
+>>1
+```
+
 ## 2 Promise对象的then方法
 then方法返回的是一个**新的Promise实例**（注意，不是原来那个Promise实例）。因此可以采用链式写法，即**then方法后面再调用另一个then方法**。
 
@@ -137,7 +150,69 @@ x:3
 
 ### (3)then方法的的链式写法的疑问
 当返回的是非Promise对象，且无return，那么使用then方法我们实际上得到的还是一个Promise对象，那么我们继续调用then方法是什么效果呢？
-是否是一种异步函数转化为同步的写法？即必须等待上一个then中函数回调结束之后，才能进行下一个then方法中运算？
+Promise对象可以作为一种异步函数转化为同步的写法，但是必须return一个Promise对象，这样就可以实现等待上一个then中函数回调结束之后，才能进行下一个then方法中运算。
+
+需要注意的是 setTimeOut函数如果采用这种写法时在走到setTimeOut那一行时，就已经开始计时了，并不会等他走完才走resolve.
+```
+ return new Promise((resolve, reject) => {
+         setTimeout(() => {
+             console.log('second then')             
+         }, 5 * 1000);
+         resolve();
+     })
+```
+只有使用以下写法，将resolve函数包含在其中，才可以保证在setTimeOut的时间走完之后，才进行之后的then所调用的程序。
+```
+ promise.then(() => {
+    let b = 1;
+    console.log('first then:' + b)
+    b = 2;
+    return new Promise((resolve, reject) => {
+         setTimeout(() => {
+             console.log('second then')   
+             resolve1();          
+         }, 5 * 1000);         
+     }).then(resolve1();
+     ).catch()
+```
+
+## 3、Promise对象的catch方法
+Promise 内部的错误不会影响到 Promise 外部的代码，通俗的说法就是“Promise 会吃掉错误”。
+
+一般总是建议，Promise 对象后面要跟catch方法，这样可以处理 Promise 内部发生的错误。catch方法返回的还是一个 Promise 对象，因此后面还可以接着调用then方法。
+
+```
+Promise.resolve()
+    .catch(function (error) {
+        console.log('oh no', error);
+    })
+    .then(function () {
+        console.log(x);
+    }).catch(
+        (err) => { throw new Error('something wrong') }
+    ).catch((err) => { console.log(err) })
+```
+在catch之中也可以对抛出错误，只有后面还跟有catch方法，就可以对抛出的错误进行捕获。
+
+为了保证Promise中的错误不会影响总体进行，所以一般在使用Promise对象时，**一定要跟一个catch方法**。
+
+## 4、Promise对象的finally方法
+finally方法用于指定不管 Promise 对象最后状态如何，都会执行的操作。
+```
+promise
+.then(result => {···})
+.catch(error => {···})
+.finally(() => {···});
+```
+上面代码中，不管promise最后的状态，在执行完then或catch指定的回调函数以后，都会执行finally方法指定的回调函数。
+
+finally方法的回调函数不接受任何参数，这意味着没有办法知道，前面的 Promise 状态到底是fulfilled还是rejected。这表明，finally方法里面的操作，应该是与状态无关的，不依赖于 Promise 的执行结果
 
 
+## 5、几种接受一组Promise对象实例作为输入的方法
+名称|方法
+-|-
+Promise.all()|所有对象都fulfilled,或有一个对象rejected;执行回调resolve(),或执行reject()
+Promise.race()|有一个对象的状态变化，该对象的状态传入回调
+Promise.allSettled()|等到所有对象的状态完成了变化，该组对象的状态结果作为一个数组返回
 
--- test_code/test_promise.js
+++ test_code/test_promise.js
@@ -1,4 +1,5 @@
-// //================= （1）then中返回的是非Promise对象===================
+//================= （1）then中返回的是非Promise对象===================
+// let a = 1;
 // const promise = new Promise((resolve, reject) => {
 //     console.log('a' + a)
 //     resolve()
@@ -6,33 +7,87 @@
 
 // promise.then(() => {
 //     let b = 1;
-//     console.log('b:' + b)
+//     console.log('first then:' + b)
 //     b = 2;
-//     return b
-// }).then((c) => {
-//     console.log('c:' + c)
+//     return
+// }).then(() => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('second then')
+//             resolve();
+//         }, 5 * 1000);
+//     })
+// }).then(() => {
+//     console.log('third then')
 // })
 
 
-//=============== （2）then中返回的是Promise对象=====================
-const promise = new Promise((resolve, reject) => {
-    resolve()
-})
+// //=============== （2）then中返回的是Promise对象=====================
+// const promise = new Promise((resolve, reject) => {
+//     resolve()
+// })
 
-var a = (b) => {
-    console.log('this is function a ')
-}
+// var a = (b) => {
+//     console.log('this is function a ')
+// }
 
-promise.then(() => {
-    let b = 1;
-    console.log('b1:' + b)
-    b = 2;
-    return new Promise((resolve, reject) => {
-        a(1)
-        resolve(1)
+// promise.then(() => {
+//     let b = 1;
+//     console.log('b1:' + b)
+//     b = 2;
+//     return new Promise((resolve, reject) => {
+//         a(1)
+//         resolve(1)
+//     })
+// }).then((x) => {
+//     console.log('x:' + (x + 2))
+// }).catch((err) => {
+//     console.log('err:' + err)
+// })
+
+// (3)then的链式写法
+// let a = 1;
+// const promise = new Promise((resolve, reject) => {
+//     console.log('a' + a)
+//     resolve()
+// })
+
+// promise.then(() => {
+//     let b = 1;
+//     console.log('first then:' + b)
+//     b = 2;
+//     return
+// }).then(() => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('second then')
+//             resolve();
+//         }, 5 * 1000);
+//     })
+// }).then(() => {
+//     console.log('third then')
+// })
+
+
+// 测试catch方法
+// let promise = new Promise((resolve,reject)=>{
+//     resolve(x)
+// })
+
+// promise.then((x)=>{
+//     console.log(x)
+// }).catch(
+//     ()=>{
+//         console.log('something wrong')
+//     }
+// )
+
+Promise.resolve()
+    .catch(function (error) {
+        console.log('oh no', error);
     })
-}).then((x) => {
-    console.log('x:' + (x + 2))
-}).catch((err) => {
-    console.log('err:' + err)
-})
+    .then(function () {
+        console.log(x);
+    }).catch(
+        (err) => { throw new Error('something wrong') }
+    ).catch((err) => { console.log(err) })
